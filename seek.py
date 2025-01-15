0c8a8030a6111cc0081c0514205ecf4764c2d08b
Skullking2520 <hyunjunsoo1@gmail.com>
2025-01-15 15:47:59 +0800

    Update seek.py

--- seek.py
+++ seek.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import re
 import csv
 import time
 
 URL = "https://www.seek.com.au/jobs/in-Victoria"
-driver = webdriver.Chrome()
+
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+service = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=options)
 
 def next_button_exists():
     try:
